b88a612b5bf586880c7b070dbe6787c6d0bcf089
Jason Acuna <jason@local>
2020-10-30 11:51:34 -0600

    Adding custom theme

---
+++ web/wp-content/themes/custom/facta/functions.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+/**
+ * If you are installing Timber as a Composer dependency in your theme, you'll need this block
+ * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
+ * plug-in, you can safely delete this block.
+ */
+$rootPath = realpath(  $_SERVER['DOCUMENT_ROOT'] . '/..' );
+$composer_autoload = $rootPath . '/vendor/autoload.php';
+define('FACTA_THEME_URI', get_template_directory_uri());
+define('FACTA_VERSION', '1.0.0');
+
+if ( file_exists( $composer_autoload ) ) {
+	require_once $composer_autoload;
+	$timber = new Timber\Timber();
+}
+
+/**
+ * This ensures that Timber is loaded and available as a PHP class.
+ * If not, it gives an error message to help direct developers on where to activate
+ */
+if ( ! class_exists( 'Timber' ) ) {
+
+	add_action(
+		'admin_notices',
+		function() {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		}
+	);
+
+	add_filter(
+		'template_include',
+		function( $template ) {
+			return get_stylesheet_directory() . '/static/no-timber.html';
+		}
+	);
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array( 'templates', 'views' );
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+Timber::$autoescape = false;
+
+require_once 'lib/Site.php';
+
+new Facta\Site();
+
---
+++ web/wp-content/themes/custom/facta/gulpfile.js
@@ -0,0 +1,109 @@
+/**
+ * A simple Gulp 4 Starter Kit for modern web development.
+ *
+ * gulpfile.js
+ *
+ * The gulp configuration file.
+ *
+ */
+
+const gulp                      = require('gulp'),
+      del                       = require('del'),
+      sourcemaps                = require('gulp-sourcemaps'),
+      plumber                   = require('gulp-plumber'),
+      sass                      = require('gulp-sass'),
+      less                      = require('gulp-less'),
+      stylus                    = require('gulp-stylus'),
+      autoprefixer              = require('gulp-autoprefixer'),
+      minifyCss                 = require('gulp-clean-css'),
+      babel                     = require('gulp-babel'),
+      webpack                   = require('webpack-stream'),
+      uglify                    = require('gulp-uglify'),
+      concat                    = require('gulp-concat'),
+      browserSync               = require('browser-sync').create(),
+      dependents                = require('gulp-dependents'),
+
+      src_folder                = './src/',
+      src_assets_folder         = src_folder + 'assets/',
+      dist_folder               = './dist/',
+      dist_assets_folder        = dist_folder + 'assets/',
+      node_modules_folder       = './node_modules/',
+      dist_node_modules_folder  = dist_folder + 'node_modules/',
+
+      node_dependencies         = Object.keys(require('./package.json').dependencies || {});
+
+gulp.task('clear', () => del([ dist_folder ]));
+
+
+
+gulp.task('sass', () => {
+  return gulp.src([
+     './sass/*.scss'
+  ],)
+    .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(dependents())
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(minifyCss())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist_folder + 'css'))
+    .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('js', () => {
+  return gulp.src([ 'js/**/*.js','!js/admin.js'])
+    .pipe(plumber())
+    .pipe(webpack({
+      mode: 'production'
+    }))
+    .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: [ '@babel/env' ]
+      }))
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist_folder + 'js'))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('vendor', () => {
+  if (node_dependencies.length === 0) {
+    return new Promise((resolve) => {
+      console.log("No dependencies specified");
+      resolve();
+    });
+  }
+
+  return gulp.src(node_dependencies.map(dependency => node_modules_folder + dependency + '/**/*.*'), {
+    base: node_modules_folder,
+    since: gulp.lastRun('vendor')
+  })
+    .pipe(gulp.dest(dist_node_modules_folder))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('build', gulp.series('clear',  'sass', 'js', 'vendor'));
+
+
+gulp.task('serve', () => {
+  return browserSync.init({
+    server: {
+      baseDir: [ 'dist' ]
+    },
+    port: 3000,
+    open: false
+  });
+});
+
+gulp.task('watch', () => {
+  
+   gulp.watch("sass", gulp.series('sass')).on('change', browserSync.reload);
+   gulp.watch("js", gulp.series('js')).on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')));
